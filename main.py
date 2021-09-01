1aa78a358ce536235639affc5708ce6b0e0d905f
Gurminder Singh <83048956+gurminder234@users.noreply.github.com>
2021-09-01 20:22:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import serial  # Serial imported for Serial communication
+import time  # Required to use delay functions
+import pyautogui
+
+ArduinoSerial = serial.Serial('com5', 9600)  # Create Serial port object called arduinoSerialData
+time.sleep(2)
+
+while 1:
+    incoming = str(ArduinoSerial.readline())  # read
+    print(incoming)
+
+
+    if 'Play/Pause' in incoming:
+        pyautogui.typewrite(['space'], 0.2)
+
+    if 'Rewind' in incoming:
+        pyautogui.hotkey('ctrl', 'left')
+
+    if 'Forward' in incoming:
+        pyautogui.hotkey('ctrl', 'right')
+
+    if 'Vup' in incoming:
+        pyautogui.hotkey('ctrl', 'down')
+
+    if 'Vdown' in incoming:
+        pyautogui.hotkey('ctrl', 'up')
+
+    incoming = "";
